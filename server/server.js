32cdcde5b79d92376815f08b0d28edc077e965fb
wij <41879253+wijiler@users.noreply.github.com>
2021-11-22 13:36:51 -0500

    feat: footer and cool backend stuff

--- server/server.js
+++ server/server.js
@@ -14,4 +14,4 @@ const port = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("public"));
 
-app.listen(port, () => console.log(`Listening on port ${port}`))
+app.listen(port, () => console.log(`Listening on port ${port}`)) //change
